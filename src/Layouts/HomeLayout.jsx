992384556b38294f202b6cee1ded9fecb119831b
Rabbi Khan <Krabby2244@gmail.com>
2025-10-20 01:38:37 +0600

    header file done

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -9,6 +9,7 @@ const HomeLayout = () => {
                 <Header></Header>
             </header>
             <main>
+                
                 <section className='left_nav'></section>
                 <section className='main'>
                     <Outlet></Outlet>
